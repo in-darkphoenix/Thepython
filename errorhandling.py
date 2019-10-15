c36d5c1995367c1fe46d382837b1104024417893
Ankit <ph.official.ankit@gmail.com>
2019-10-15 20:56:54 +0530

    This is the first commit

---
+++ errorhandling.py
@@ -0,0 +1,16 @@
+print(5)
+int(9)
+int(6)
+print(70)
+print(69)
+a = 5
+b = "6"
+print(int(6.9))
+print(str(a)+b)
+def div(x,y):
+    try:
+        return(x/y)
+    except ZeroDivisionError:
+        return "Division with 0 is meaningless"
+
+print(div(1,7))
